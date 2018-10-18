4ffba1990bb6d4565f2e3e2847b1fbf7d4eb4b82
J.C. Beljaars <jeroen.beljaars@hva.nl>
2018-10-18 19:34:50 +0200

    added assignment 3

---
+++ Assignment-3-student/src/main/java/nl/hva/ict/ds/util/DoubleHashingMultiValueSymbolTable.java
@@ -0,0 +1,20 @@
+package nl.hva.ict.ds.util;
+
+import nl.hva.ict.ds.Player;
+
+import java.util.List;
+
+public class DoubleHashingMultiValueSymbolTable implements MultiValueSymbolTable<String, Player> {
+    public DoubleHashingMultiValueSymbolTable(int arraySize) {
+    }
+
+    @Override
+    public void put(String key, Player value) {
+
+    }
+
+    @Override
+    public List<Player> get(String key) {
+        return null;
+    }
+}
---
+++ Assignment-3-student/src/main/java/nl/hva/ict/ds/util/LinearProbingMultiValueSymbolTable.java
@@ -0,0 +1,20 @@
+package nl.hva.ict.ds.util;
+
+import nl.hva.ict.ds.Player;
+
+import java.util.List;
+
+public class LinearProbingMultiValueSymbolTable implements MultiValueSymbolTable<String, Player> {
+    public LinearProbingMultiValueSymbolTable(int arraySize) {
+    }
+
+    @Override
+    public void put(String key, Player value) {
+
+    }
+
+    @Override
+    public List<Player> get(String key) {
+        return null;
+    }
+}
