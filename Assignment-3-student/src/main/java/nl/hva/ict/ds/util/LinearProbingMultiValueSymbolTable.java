290bb06481965b014419dc465f3923bcde4f72bb
michaelfontijn <mich207hael@hotmail.com>
2018-10-20 13:34:33 +0200

    LinearProbing should be done i gues ;p

--- Assignment-3-student/src/main/java/nl/hva/ict/ds/util/LinearProbingMultiValueSymbolTable.java
+++ Assignment-3-student/src/main/java/nl/hva/ict/ds/util/LinearProbingMultiValueSymbolTable.java
@@ -2,66 +2,100 @@ package nl.hva.ict.ds.util;
 
 import nl.hva.ict.ds.Player;
 
+import java.util.ArrayList;
 import java.util.Hashtable;
 import java.util.List;
 
 public class LinearProbingMultiValueSymbolTable implements MultiValueSymbolTable<String, Player> {
 
-    Hashtable<Integer,Node> table = new Hashtable<Integer, Node>();
+    Chain[] table;
+
 
     public LinearProbingMultiValueSymbolTable(int arraySize) {
-
+        //initialize the array size //TODO the static size is temp for test, but idk why the unit test class initializes the array with an size of 7, this is ofc never goign to work..
+        table = new Chain[10501];
     }
 
     @Override
     public void put(String key, Player value) {
-        int hashingKey = value.getFirstName().hashCode();
+        int desiredIndex = customHash(key);
 
-        //if the table already contains the key
-        if(table.containsKey(hashingKey)){
+        if(desiredIndex < 0){
+            //do something
+            System.out.println("lel");
+        }
 
-            //get the node that is at the index of the hashkey
-            Node node = table.get(hashingKey);
+        //check if there already is an index on the desired index
+        if(table[desiredIndex] != null){
+            //now find the last node in the chain, and append the new node to the end of the chain
+            Node node =table[desiredIndex].firsNode;
 
-            //find the last node in the current chain on the index of the hashingkey
             while(node.nextNode != null){
                 node = node.nextNode;
             }
 
-            //when the last node is found, add the new node to the end of the link chain
+            //when the last node is found, append the new node here
             node.nextNode = new Node(value);
-
-
         }
-        //else just insert it normally
         else{
-            table.put(value.getFirstName().hashCode(), new Node(value));
+            table[desiredIndex] = new Chain(new Node(value));
         }
+
 
     }
 
     @Override
     public List<Player> get(String key) {
+        List<Player> result =  new ArrayList<>();
 
-        //convert the "key" wich is firtname to an hashkey
-        //check if there is a node chain on the index of the hashkey
-        //if there is no chain just return the player value of the node.
-        //if there is a chain on the index of the hashkey, return all player in the chain
+        //get the index by using the same hashing algorithm on the key that was used in the put
+        int index = customHash(key);
 
-        return null;
+        //Check weather there is something on the index at all
+        if(table[index].firsNode != null){
+
+            //if there is something on the index, add at least the first node
+            Node node = table[index].firsNode;
+            result.add(node.value);
+
+            //now add all the nodes of the chain to the result
+            while(node.nextNode != null){
+                node = node.nextNode;
+                result.add(node.value);
+            }
+        }
+
+        return result;
     }
 
+    /***
+     * Creates a hash from a string
+     * @param stringToHash The string to be hashed
+     * @return a hash of type int
+     */
+    private int customHash(String stringToHash){
+        int result = stringToHash.hashCode() % 31;
 
+        //for some reason the hashCode method sometimes creates negative hashes, just make it absolute to get a positive int
+        if(result < 0) result = Math.abs(result);
+
+        return result;
+    }
 }
 
-class LinkedList{
+class Chain {
     Node firsNode;
+
+    public Chain(){}
+
+    public Chain(Node firsNode){
+        this.firsNode = firsNode;
+    }
 }
 
 class Node{
     Node nextNode;
     Player value;
-
 
     public Node(Player value){
         this.value = value;
--- Assignment-3-student/src/test/java/nl/hva/ict/ds/ExtendedPlayerFinderTest.java
+++ Assignment-3-student/src/test/java/nl/hva/ict/ds/ExtendedPlayerFinderTest.java
@@ -1,7 +1,19 @@
 package nl.hva.ict.ds;
 
+import nl.hva.ict.ds.util.LinearProbingMultiValueSymbolTable;
+import org.junit.Test;
+
 /**
  * If you have any tests that should be overwritten or added please put them to this class.
  */
 public class ExtendedPlayerFinderTest extends HighScorePlayerFinderTest {
+
+    @Test
+    public void doMagic(){
+        Player nearlyHeadlessNick = new Player("Nicholas", "de Mimsy-Porpington", 95);
+
+        LinearProbingMultiValueSymbolTable sb = new LinearProbingMultiValueSymbolTable(10501);
+        sb.put(nearlyHeadlessNick.getFirstName(), nearlyHeadlessNick);
+    }
+
 }
