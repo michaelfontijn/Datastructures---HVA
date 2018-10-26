5792fb2d085f197815834467b8358848d35bbbaf
michaelfontijn <mich207hael@hotmail.com>
2018-10-26 13:14:12 +0200

    soms

--- Assignment-3-student/src/main/java/nl/hva/ict/ds/util/LinearProbingMultiValueSymbolTable.java
+++ Assignment-3-student/src/main/java/nl/hva/ict/ds/util/LinearProbingMultiValueSymbolTable.java
@@ -13,12 +13,14 @@ public class LinearProbingMultiValueSymbolTable implements MultiValueSymbolTable
 
     public LinearProbingMultiValueSymbolTable(int arraySize) {
         //initialize the array size //TODO the static size is temp for test, but idk why the unit test class initializes the array with an size of 7, this is ofc never goign to work..
-        table = new Player[10501];
+        table = new Player[arraySize];
     }
 
     @Override
     public void put(String key, Player value) {
         int desiredIndex = customHash(key);
+
+        if(desiredIndex > table.length-1) return;
 
         //check if there already is an index on the desired index, find the next open index
         if(table[desiredIndex] != null){
@@ -48,9 +50,10 @@ public class LinearProbingMultiValueSymbolTable implements MultiValueSymbolTable
 
         //find a match with the key
         for(int i = startIndex; i < table.length; i++){
+            if(table[i] == null) return result;
+
             if(table[i].getFirstName() == key){
                 result.add(table[i]);
-                return result;
             }
         }
         return result;
@@ -62,7 +65,7 @@ public class LinearProbingMultiValueSymbolTable implements MultiValueSymbolTable
      * @return a hash of type int
      */
     private int customHash(String stringToHash){
-        int result = stringToHash.hashCode() % 31;
+        int result = stringToHash.hashCode() % (table.length -1);
 
         //for some reason the hashCode method sometimes creates negative hashes, just make it absolute to get a positive int
         if(result < 0) result = Math.abs(result);
